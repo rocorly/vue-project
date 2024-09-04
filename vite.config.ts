0853e7320439d7cb82479f31d1f72186a5f2a4d8
rocorly <143928467+rocorly@users.noreply.github.com>
2024-09-04 11:36:39 +1000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/vue-project/',
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
