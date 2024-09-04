f440841ac7324e536a42083fd03106927e1039bc
rocorly <143928467+rocorly@users.noreply.github.com>
2024-09-04 11:15:56 +1000

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,29 @@
 <script lang="ts">
 export default {
- name: 'HelloWorld',
- props: {
- msg: String
- },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
   data() {
     return {
       theData: {}
     }
   },
- methods: {
- fetchData() {
+  methods: {
+    fetchData() {
       // use a piece of dynamic data to modify the API call
       let query = 'https://api.collection.nfsa.gov.au/search?query=' + this.msg;
       console.log(query);
- fetch(query)
- .then(response => {
- // response.json().then(res => console.log(res));
+      fetch(query)
+        .then(response => {
+          // response.json().then(res => console.log(res));
           response.json().then(res => this.$data.theData = res);
- })
- .catch(err => {
- console.error(err);
- });
- }
- }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
 };
 
 let baseurl = 'https://media.nfsacollection.net/';
@@ -38,10 +38,10 @@ let imgurl;
       <!-- create a variable called result, 
       loop through the API results and add a list item for each result.
       Use result to access properties like 'title' and 'name' -->
-      <li v-for="result in theData.results" :key="result.title">
+      <!-- <li v-for="result in theData.results" :key="result.title">
         {{ result.title }}
         {{ result.name }}
-      </li>
+      </li> -->
     </ul>
   </div>
 </template>
